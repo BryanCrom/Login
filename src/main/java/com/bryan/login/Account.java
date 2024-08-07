91ea3f5b331acef81315f3f2fbc8db882f4debf3
Bryan <BryanCrombach@gmail.com>
2024-08-08 00:35:09 +1200

    feat(Database): add establishConnection, close, and checkExistingTable methods for the specialised database functionality

--- src/main/java/com/bryan/login/Account.java
+++ src/main/java/com/bryan/login/Account.java
@@ -10,4 +10,67 @@ package com.bryan.login;
  */
 public class Account {
     
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String birthDay;
+    private String username;
+    private String password;
+    
+    public Account(String firstName, String lastName, String email, String birthDay, String username, String password){
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setEmail(email);
+        this.setBirthDay(birthDay);
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/bryan/login/Database.java
+++ src/main/java/com/bryan/login/Database.java
@@ -4,10 +4,62 @@
  */
 package com.bryan.login;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author bryan
  */
-public class Database {
+public final class Database {
+    
+    private static Connection conn;
+    private static final String URL = "jdbc:derby:AccountsDB; create=true";
+    private static final String USERNAME = "LoginUsername";
+    private static final String PASSWORD = "LoginPassword";
+    
+    public Database(){
+        establishConnection();
+    }
+    
+    public void establishConnection(){
+        if(conn == null){
+            try{
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                System.out.println(URL + " connected...");
+            }
+            catch(SQLException e){
+                System.err.println("SQLException: " + e.getMessage());
+            }
+        }
+    }
+    
+    public void closeConnection(){
+        if(conn != null){
+            try{
+                conn.close();
+            }
+            catch(SQLException e){
+                System.err.println("SQLException: " + e.getMessage());
+            }
+        }
+    }
+    
+    public boolean checkTableExsisting(String tableName){
+        boolean tableExists = false;
+        try{
+            DatabaseMetaData dbmd = conn.getMetaData();
+            ResultSet rs = dbmd.getTables(null, null, tableName, null);
+            tableExists = rs.next();
+        }
+        catch(SQLException e){
+            System.err.println("SQLException: " + e.getMessage());
+        }
+        return tableExists;
+    }
+    
     
 }
